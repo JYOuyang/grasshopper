bb36daac5f7bf21d902577503f2f0f2e4037f059
James Ouyang <james@heaviside.none>
2013-08-04 18:50:46 -0700

    Handle subtraction using groupings.

--- arithmetic.py
+++ arithmetic.py
@@ -7,3 +7,22 @@ sumexpr = '1+2+3+4'
 
 print sum([int(each) for each in sumexpr.split('+')])
 
+subexpr = '1+2-3-2'
+
+addgrps = subexpr.split('+')
+
+def intsub(grp):
+    try:
+        grp = int(grp.strip())
+
+    except:
+        grp = [int(elem) for elem in grp.split('-')]
+
+        grp = grp[0] - sum(grp[1:])
+
+    return grp
+
+out = map(intsub, addgrps)
+
+print sum(out)
+
